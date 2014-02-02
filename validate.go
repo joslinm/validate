bc63d7b3acfa0f9876661a69ae13258c4f77399b
Mark Joslin <mjoslin@gmail.com>
2014-02-02 11:41:26 -0500

    update constants, rules, use goblin tests

--- validate.go
+++ validate.go
@@ -1,23 +1,23 @@
 package validate
 
 import (
-  "errors"
-  "fmt"
-  "github.com/lann/builder"
-  _ "net/http"
-  "reflect"
-  "regexp"
-  "time"
+	"errors"
+	"fmt"
+	"github.com/lann/builder"
+	"log"
+	_ "net/http"
+	"reflect"
+	"regexp"
+	"time"
 )
 
 // Types validate supports
 const (
-  STRING = "STRING"
-  NUMBER = "NUMBER"
-  INT    = "INT"
-  FLOAT  = "FLOAT"
-  BOOL   = "BOOL"
-  TIME   = "TIME"
+	Unknown = iota
+	String
+	Number
+	Bool
+	Time
 )
 
 // Type of callbacks to be used in a Rule
@@ -27,268 +27,363 @@ type CustomCallback func(value interface{}) bool
 
 // Rule encompasses a single validation rule for a parameter
 type Rule struct {
-  Key      string
-  Is       string
-  Required bool
-  Regex    string
-  Min      interface{}
-  Max      interface{}
+	Key      string
+	Type     int
+	Required bool
+	Regex    string
+	Message  string
+	Min      interface{}
+	Max      interface{}
 
-  // callbacks
-  Customs  []CustomCallback
-  Prepares []PrepareCallback
-  Alters   []AlterCallback
+	// callbacks
+	Customs  []CustomCallback
+	Prepares []PrepareCallback
+	Alters   []AlterCallback
 }
 
 type ruleBuilder builder.Builder
 type RuleBook map[string]ruleBuilder
 
 type ValidationData struct {
-  data interface{}
+	data interface{}
 }
 
 func Data(data interface{}) *ValidationData {
-  fmt.Println("Validation data: ", data)
-  return &ValidationData{data: data}
+	fmt.Println("Validation data: ", data)
+	return &ValidationData{data: data}
 }
 
 func (v *ValidationData) With(rules RuleBook) (interface{}, error) {
-  fmt.Printf("Validation data type: %v", reflect.TypeOf(v.data))
-  if _, ok := v.data.(map[string]interface{}); ok {
-    return Map(v.data.(map[string]interface{}), rules)
-  }
-  return nil, errors.New("Unrecognized input data")
-}
-
-func (rb ruleBuilder) updateTypeTo(t string) ruleBuilder {
-  return builder.Set(rb, "Is", t).(ruleBuilder)
+	fmt.Printf("Validation data type: %v", reflect.TypeOf(v.data))
+	if _, ok := v.data.(map[string]interface{}); ok {
+		return Map(v.data.(map[string]interface{}), rules)
+	}
+	return nil, errors.New("Unrecognized input data")
 }
 
 func (rb ruleBuilder) Build() Rule {
-  s := builder.GetStruct(rb).(Rule)
-  fmt.Println("\nGot struct: ", s)
-  return builder.GetStruct(rb).(Rule)
+	s := builder.GetStruct(rb).(Rule)
+	fmt.Println("\nGot struct: ", s)
+	return builder.GetStruct(rb).(Rule)
 }
 
 // required / optional
 func (rb ruleBuilder) Required() ruleBuilder {
-  return builder.Set(rb, "Required", true).(ruleBuilder)
+	return builder.Set(rb, "Required", true).(ruleBuilder)
 }
 
 // key
 func (rb ruleBuilder) Key(key string) ruleBuilder {
-  return builder.Set(rb, "Key", key).(ruleBuilder)
+	return builder.Set(rb, "Key", key).(ruleBuilder)
 }
 
 // type
-func (rb ruleBuilder) Is(is string) ruleBuilder {
-  return builder.Set(rb, "Is", is).(ruleBuilder)
+func (rb ruleBuilder) Type(is string) ruleBuilder {
+	return builder.Set(rb, "Type", is).(ruleBuilder)
 }
 func (rb ruleBuilder) String() ruleBuilder {
-  return builder.Set(rb, "Is", STRING).(ruleBuilder)
+	return builder.Set(rb, "Type", String).(ruleBuilder)
 }
 func (rb ruleBuilder) Number() ruleBuilder {
-  return builder.Set(rb, "Is", NUMBER).(ruleBuilder)
+	return builder.Set(rb, "Type", Number).(ruleBuilder)
 }
 func (rb ruleBuilder) Bool() ruleBuilder {
-  return builder.Set(rb, "Is", BOOL).(ruleBuilder)
+	return builder.Set(rb, "Type", Bool).(ruleBuilder)
+}
+func (rb ruleBuilder) Time() ruleBuilder {
+	return builder.Set(rb, "Type", Time).(ruleBuilder)
+}
+
+// message
+func (rb ruleBuilder) Message(msg string) ruleBuilder {
+	return builder.Set(rb, "Message", msg).(ruleBuilder)
 }
 
 // regex
 func (rb ruleBuilder) Regex(regex string) ruleBuilder {
-  b := rb.updateTypeTo(STRING)
-  return builder.Set(b, "Regex", regex).(ruleBuilder)
+	return builder.Set(rb, "Regex", regex).(ruleBuilder)
+}
+
+func (rb ruleBuilder) updateTypeAccordingTo(val interface{}) ruleBuilder {
+	// get type
+	t := reflect.TypeOf(val)
+	log.Println("Dynamically updating type from", t.Kind().String())
+
+	switch t.Kind() {
+
+	// primitives
+	case reflect.Bool:
+		log.Println("Updating type to number")
+		rb = rb.Bool()
+		break
+	case reflect.Int:
+		fallthrough
+	case reflect.Int8:
+		fallthrough
+	case reflect.Int16:
+		fallthrough
+	case reflect.Int32:
+		fallthrough
+	case reflect.Int64:
+		fallthrough
+	case reflect.Uint:
+		fallthrough
+	case reflect.Uint8:
+		fallthrough
+	case reflect.Uint16:
+		fallthrough
+	case reflect.Uint32:
+		fallthrough
+	case reflect.Uint64:
+		fallthrough
+	case reflect.Uintptr:
+		fallthrough
+	case reflect.Float32:
+		fallthrough
+	case reflect.Float64:
+		fallthrough
+	case reflect.Complex64:
+		fallthrough
+	case reflect.Complex128:
+		log.Println("Updating type to number")
+		rb = rb.Number()
+		break
+	case reflect.Array:
+	case reflect.Chan:
+	case reflect.Func:
+	case reflect.Interface:
+	case reflect.Map:
+	case reflect.Ptr:
+	case reflect.Slice:
+	case reflect.String:
+	case reflect.Struct:
+		if _, ok := val.(time.Time); ok {
+			log.Println("Updating type to time")
+			rb = rb.Time()
+		}
+		break
+
+	case reflect.UnsafePointer:
+	default:
+		panic("Do not understand this type: " + reflect.TypeOf(val).Kind().String())
+	}
+
+	return rb
+}
+
+func sameType(vals ...interface{}) bool {
+	expectedType := reflect.TypeOf(vals[0]).Kind()
+	for _, val := range vals {
+		if expectedType != reflect.TypeOf(val).Kind() {
+			return false
+		}
+	}
+
+	return true
+}
+
+func (rb ruleBuilder) ensureMinAndMaxEqualTypes() {
+	rule := rb.Build()
+	if rule.Max != nil && rule.Min != nil { // if max/min set
+		if !sameType(rule.Max, rule.Min) {
+			panic("You set min/max rules to different types")
+		}
+	}
 }
 
 // min, max, between
-func (rb ruleBuilder) Min(min int) ruleBuilder {
-  rb = rb.updateTypeTo(NUMBER)
-  return builder.Set(rb, "Min", min).(ruleBuilder)
+func (rb ruleBuilder) Min(min interface{}) ruleBuilder {
+	rb = builder.Set(rb, "Min", min).(ruleBuilder)
+	rb.ensureMinAndMaxEqualTypes()
+	rb = rb.updateTypeAccordingTo(min)
+	return rb
 }
 
-func (rb ruleBuilder) Max(max int) ruleBuilder {
-  rb = rb.updateTypeTo(NUMBER)
-  return builder.Set(rb, "Max", max).(ruleBuilder)
+func (rb ruleBuilder) Max(max interface{}) ruleBuilder {
+	rb = builder.Set(rb, "Max", max).(ruleBuilder)
+	rb.ensureMinAndMaxEqualTypes()
+	rb = rb.updateTypeAccordingTo(max)
+	return rb
 }
 
-func (rb ruleBuilder) Between(min int, max int) ruleBuilder {
-  rb = rb.updateTypeTo(NUMBER)
-  builder.Set(rb, "Min", min)
-  builder.Set(rb, "Max", max)
+func (rb ruleBuilder) Between(min interface{}, max interface{}) ruleBuilder {
+	if reflect.TypeOf(min).Kind() != reflect.TypeOf(max).Kind() {
+		panic("Disparate values passed into Between(...) \n" +
+			"\nMin: " + reflect.TypeOf(min).Kind().String() +
+			"\nMax: " + reflect.TypeOf(max).Kind().String())
+	}
+	rb = rb.updateTypeAccordingTo(min)
+	builder.Set(rb, "Min", min)
+	builder.Set(rb, "Max", max)
 
-  return rb
+	return rb
 }
 
 // date
 func (rb ruleBuilder) Before(time time.Time) ruleBuilder {
-  rb.updateTypeTo(TIME)
-  return builder.Set(rb, "Before", time).(ruleBuilder)
+	return builder.Set(rb, "Before", time).(ruleBuilder)
 }
 func (rb ruleBuilder) After(time time.Time) ruleBuilder {
-  rb.updateTypeTo(TIME)
-  return builder.Set(rb, "After", time).(ruleBuilder)
+	return builder.Set(rb, "After", time).(ruleBuilder)
 }
 func (rb ruleBuilder) BetweenTimes(min time.Time, max time.Time) ruleBuilder {
-  rb.updateTypeTo(NUMBER)
-  builder.Set(rb, "Before", max)
-  builder.Set(rb, "After", min)
+	builder.Set(rb, "Before", max)
+	builder.Set(rb, "After", min)
 
-  return rb
+	return rb
 }
 
 // callback
 func (rb ruleBuilder) Custom(cb CustomCallback) ruleBuilder {
-  return builder.Append(rb, "Customs", cb).(ruleBuilder)
+	return builder.Append(rb, "Customs", cb).(ruleBuilder)
 }
 func (rb ruleBuilder) Alter(cb AlterCallback) ruleBuilder {
-  return builder.Append(rb, "Alters", cb).(ruleBuilder)
+	return builder.Append(rb, "Alters", cb).(ruleBuilder)
 }
 func (rb ruleBuilder) Prepare(cb PrepareCallback) ruleBuilder {
-  return builder.Append(rb, "Prepares", cb).(ruleBuilder)
+	return builder.Append(rb, "Prepares", cb).(ruleBuilder)
 }
 
 // custom
 func (rb ruleBuilder) Email() ruleBuilder {
-  return builder.Set(rb, "Regex", `(?i)[A-Z0-9._%+-]+@(?:[A-Z0-9-]+\.)+[A-Z]{2,6}`).(ruleBuilder)
+	return builder.Set(rb, "Regex", `(?i)[A-Z0-9._%+-]+@(?:[A-Z0-9-]+\.)+[A-Z]{2,6}`).(ruleBuilder)
 }
 
 var RuleBuilder = builder.Register(ruleBuilder{}, Rule{}).(ruleBuilder)
 
 func (rule *Rule) checkAgainst(given map[string]interface{}) (interface{}, error) {
-  fmt.Printf("\nChecking \"%v\"", rule.Key)
-  fmt.Printf("\n--------")
+	fmt.Printf("\nChecking \"%v\"", rule.Key)
+	fmt.Printf("\n--------")
 
-  fmt.Printf("\nRequired...")
-  val, ok := given[rule.Key]
-  if !ok {
-    if rule.Required {
-      // Throw error indicating value is not in given input
-      fmt.Printf("FAIL")
-      return false, errors.New("Required key not found")
-    } else {
-      fmt.Printf("SKIP")
-      return true, nil
-    }
-  } else {
-    fmt.Printf("OK")
-  }
+	fmt.Printf("\nRequired...")
+	val, ok := given[rule.Key]
+	if !ok {
+		if rule.Required {
+			// Throw error indicating value is not in given input
+			fmt.Printf("FAIL")
+			return false, errors.New("Required key not found")
+		} else {
+			fmt.Printf("SKIP")
+			return true, nil
+		}
+	} else {
+		fmt.Printf("OK")
+	}
 
-  fmt.Printf("\nType... %v", reflect.TypeOf(val))
+	fmt.Printf("\nType... %v", reflect.TypeOf(val))
 
-  fmt.Printf("\nCustom...")
-  if len(rule.Customs) > 0 {
-    for _, cb := range rule.Customs {
-      if ok := cb(val); !ok {
-        fmt.Printf("FAIL")
-        return false, errors.New("Custom failed")
-      } else {
-        fmt.Printf("OK")
-      }
-    }
-  } else {
-    fmt.Printf("SKIP")
-  }
+	fmt.Printf("\nCustom...")
+	if len(rule.Customs) > 0 {
+		for _, cb := range rule.Customs {
+			if ok := cb(val); !ok {
+				fmt.Printf("FAIL")
+				return false, errors.New("Custom failed")
+			} else {
+				fmt.Printf("OK")
+			}
+		}
+	} else {
+		fmt.Printf("SKIP")
+	}
 
-  fmt.Printf("\nAlters...")
-  if len(rule.Alters) > 0 {
-    for _, cb := range rule.Alters {
-      val = cb(val)
-    }
-  } else {
-    fmt.Printf("SKIP")
-  }
+	fmt.Printf("\nAlters...")
+	if len(rule.Alters) > 0 {
+		for _, cb := range rule.Alters {
+			val = cb(val)
+		}
+	} else {
+		fmt.Printf("SKIP")
+	}
 
-  fmt.Printf("\nRegex...")
-  if len(rule.Regex) > 0 {
-    re, err := regexp.Compile(rule.Regex)
-    if err != nil {
-      fmt.Printf("Invalid regex! %v", err)
-      return false, errors.New("Invalid regex")
-    }
-    if re.MatchString(val.(string)) {
-      fmt.Printf("OK")
-    } else {
-      fmt.Printf("FAIL")
-      return false, nil
-    }
-  } else {
-    fmt.Printf("SKIP")
-  }
+	fmt.Printf("\nRegex...")
+	if len(rule.Regex) > 0 {
+		re, err := regexp.Compile(rule.Regex)
+		if err != nil {
+			fmt.Printf("Invalid regex! %v", err)
+			return false, errors.New("Invalid regex")
+		}
+		if re.MatchString(val.(string)) {
+			fmt.Printf("OK")
+		} else {
+			fmt.Printf("FAIL")
+			return false, nil
+		}
+	} else {
+		fmt.Printf("SKIP")
+	}
 
-  fmt.Printf("\nType...")
-  if len(rule.Is) > 0 {
-    ok := true
-    switch rule.Is {
-    case STRING:
-      _, ok = val.(string)
-      break
-    }
-    if ok {
-      fmt.Printf("OK")
-    } else {
-      fmt.Printf("FAIL")
-    }
+	fmt.Printf("\nType...")
+	if rule.Type > 0 {
+		ok := true
+		if rule.Type == String {
+			_, ok = val.(string)
+		}
+		if ok {
+			fmt.Printf("OK")
+		} else {
+			fmt.Printf("FAIL")
+		}
 
-  } else {
-    fmt.Printf("SKIP")
-  }
+	} else {
+		fmt.Printf("SKIP")
+	}
 
-  fmt.Printf("\n--------\n")
+	fmt.Printf("\n--------\n")
 
-  return val, nil
+	return val, nil
 }
 
 func RuleBookFor(obj interface{}, required bool) RuleBook {
-  fmt.Println("\nGot obj ", obj)
+	fmt.Println("\nGot obj ", obj)
 
-  typ := reflect.TypeOf(obj)
-  // if a pointer to a struct is passed, get the type of the dereferenced object
-  if typ.Kind() == reflect.Ptr {
-    typ = typ.Elem()
-  }
-  fmt.Println("\nGot obj ", typ)
-  ruleBook := make(RuleBook)
+	typ := reflect.TypeOf(obj)
+	// if a pointer to a struct is passed, get the type of the dereferenced object
+	if typ.Kind() == reflect.Ptr {
+		typ = typ.Elem()
+	}
+	fmt.Println("\nGot obj ", typ)
+	ruleBook := make(RuleBook)
 
-  for i := 0; i < typ.NumField(); i++ {
-    p := typ.Field(i)
-    var rb ruleBuilder
+	for i := 0; i < typ.NumField(); i++ {
+		p := typ.Field(i)
+		var rb ruleBuilder
 
-    switch p.Type.Kind() {
-    case reflect.Float64:
-    case reflect.Float32:
-    case reflect.Int:
-      rb = RuleBuilder.Number()
-      break
-    case reflect.String:
-      rb.String()
-      break
-    case reflect.Bool:
-      rb.Bool()
-      break
-    default:
-      break
-    }
-    if required {
-      ruleBook[p.Name] = rb.Required()
-    } else {
-      ruleBook[p.Name] = rb
-    }
-  }
+		switch p.Type.Kind() {
+		case reflect.Float64:
+		case reflect.Float32:
+		case reflect.Int:
+			rb = RuleBuilder.Number()
+			break
+		case reflect.String:
+			rb.String()
+			break
+		case reflect.Bool:
+			rb.Bool()
+			break
+		default:
+			break
+		}
+		if required {
+			ruleBook[p.Name] = rb.Required()
+		} else {
+			ruleBook[p.Name] = rb
+		}
+	}
 
-  return ruleBook
+	return ruleBook
 }
 
 func Map(given map[string]interface{}, expected RuleBook) (map[string]interface{}, error) {
-  retVals := make(map[string]interface{})
-  for k, v := range expected {
-    fmt.Printf("\n%v = %v", k, reflect.TypeOf(v))
-    rule := v.Key(k).Build()
-    val, err := rule.checkAgainst(given)
-    if err != nil {
-      return nil, err
-    }
-    retVals[k] = val
-  }
+	retVals := make(map[string]interface{})
+	for k, v := range expected {
+		fmt.Printf("\n%v = %v", k, reflect.TypeOf(v))
+		rule := v.Key(k).Build()
+		val, err := rule.checkAgainst(given)
+		if err != nil {
+			return nil, err
+		}
+		retVals[k] = val
+	}
 
-  return retVals, nil
+	return retVals, nil
 }
--- validate_test.go
+++ validate_test.go
@@ -1,92 +1,81 @@
 package validate_test
 
 import (
-  "fmt"
-  "github.com/joslinm/validate"
-  "testing"
+	. "github.com/franela/goblin"
+	"github.com/joslinm/validate"
+	"testing"
+	"time"
 )
 
 // request
 
-func TestValidateDataChecksCustoms(t *testing.T) {
-  did_check_callback := false
-  required := validate.RuleBuilder.Required()
-  expecting := validate.RuleBook{
-    "string": required.Regex(`\w+`).Custom(
-      func(value interface{}) bool {
-        did_check_callback = true
-        return true
-      },
-    ),
-    "int": required.Number(),
-  }
+func Test(t *testing.T) {
+	g := Goblin(t)
+	g.Describe("Rules", func() {
+		g.It("Should be capable of being constructed by builder", func() {
+			msg := "This is a test"
+			rule := validate.RuleBuilder.Required().Regex(".*").Min(5).Max(10).Message(msg).Build()
+			g.Assert(rule.Required).Equal(true)
+			g.Assert(rule.Min).Equal(5)
+			g.Assert(rule.Max).Equal(10)
+			g.Assert(rule.Message).Equal(msg)
+			g.Assert(rule.Regex).Equal(".*")
+		})
 
-  _, err := validate.Data(map[string]interface{}{"int": 123}).With(expecting)
-  if !did_check_callback {
-    fmt.Printf("\nDid not check callback")
-    t.Fail()
-  }
-  if err != nil {
-    fmt.Printf("\nError: %v", err)
-    t.Fail()
-  }
+		g.It(`Should update Type property to number when Min(...) / Max(...) / Between(...)
+          called with number`, func() {
+			rule := validate.RuleBuilder.Min(5).Build()
+			g.Assert(rule.Type).Equal(validate.Number)
+			rule = validate.RuleBuilder.Max(5).Build()
+			g.Assert(rule.Type).Equal(validate.Number)
+			rule = validate.RuleBuilder.Between(5, 10).Build()
+			g.Assert(rule.Type).Equal(validate.Number)
+		})
 
-}
+		g.It(`Should update Type property to Time when Min(...) / Max(...) / Between(...)
+          called with a time`, func() {
+			rule := validate.RuleBuilder.Min(time.Now()).Build()
+			g.Assert(rule.Type).Equal(validate.Time)
+			rule = validate.RuleBuilder.Max(time.Now()).Build()
+			g.Assert(rule.Type).Equal(validate.Time)
+			rule = validate.RuleBuilder.Between(time.Now(), time.Now()).Build()
+			g.Assert(rule.Type).Equal(validate.Time)
+		})
 
-func TestRuleBuilder(t *testing.T) {
-  rule := validate.RuleBuilder.Required().Number().Build()
-  fmt.Printf("\nrule = %v", rule)
-  if rule.Required != true {
-    fmt.Printf("\nrule.required = %v", rule.Required)
-    fmt.Printf("\nexpected = %v\n", true)
-    t.Fail()
-  }
-  if rule.Is != validate.NUMBER {
-    t.Fail()
-  }
-}
+		g.It("Should not allow disparate values passed into Between(...)", func(done Done) {
+			defer func() {
+				// catch panic..
+				g.Assert(recover() != nil).IsTrue()
+				done()
+			}()
+			validate.RuleBuilder.Between(5, time.Now()).Build()
+		})
 
-func TestRuleBuilderDifferentRules(t *testing.T) {
-  rule := validate.RuleBuilder
+		g.It("Should not allow disparate values passed into Min & Max", func(done Done) {
+			defer func() {
+				// catch panic..
+				g.Assert(recover() != nil).IsTrue()
+				done()
+			}()
+			validate.RuleBuilder.Min(5).Max(time.Now()).Build()
+		})
 
-  rules := validate.RuleBook{
-    "lc": rule.Regex("[a-z]+"),
-    "uc": rule.Regex("[A-Z]+"),
-  }
-  params, err := validate.Data(map[string]interface{}{
-    "lc":  "mark",
-    "uc":  "JOSLIN",
-    "age": 25,
-  }).With(rules)
+	})
 
-  if err != nil {
-    fmt.Printf("\nError: %v", err)
-    t.Fail()
-  }
+	g.Describe("Validation", func() {
+		g.It("Should call 'custom' callbacks", func() {
+			called_custom_cb := false
+			rule := validate.RuleBuilder.Custom(func(value interface{}) bool {
+				called_custom_cb = true
+				return true
+			})
+			validate.Data(map[string]interface{}{
+				"key": "val",
+			}).With(validate.RuleBook{
+				"key": rule,
+			})
 
-  fmt.Printf("\nParams:\n%v", params)
-}
-
-func TestRuleBookForStruct(t *testing.T) {
-  type UserParams struct {
-    // you must export struct declarations
-    first_name string
-    last_name  string
-    age        int
-  }
-
-  rules := validate.RuleBookFor(&UserParams{}, true)
-  rules["first_name"].Alter(func(value interface{}) interface{} {
-    fmt.Printf("Value: %v", value)
-    return value
-  })
-  params, _ := validate.Data(map[string]interface{}{
-    "first_name": "Mark",
-    "last_name":  "Joslin",
-    "age":        25,
-  }).With(rules)
-  fmt.Printf("Got params.. %v", params)
-}
-
-func TestNestedRuleBooks(t *testing.T) {
+			g.Assert(called_custom_cb).Equal(true)
+		})
+	})
 }
